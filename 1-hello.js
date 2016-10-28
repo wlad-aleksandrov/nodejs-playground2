070aa7679032470e68e14aa6adace387ede8f2f5
Vlad Aleksandrov <wlad.aleksandrov@gmail.com>
2016-10-28 09:26:17 +0000

    added 3-callback.js; 1-hello.js changed

--- 1-hello.js
+++ 1-hello.js
@@ -1,5 +1,14 @@
-setInterval(function(){
-    console.log("World");
-}, 1000);
+const http = require('http');
 
-console.log("Hello");
+const hostname = process.env.IP;
+const port = process.env.PORT;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
